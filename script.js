0f8ed3b4010b7ed0793c7a476a0463c938d2bed0
Black Cannary <68380237+juanesherrera21@users.noreply.github.com>
2022-10-18 11:55:43 -0500

    Nuevas funcionalidades
    Se añadio ventana main menu

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 
 
 var VectorNombres = [];
-var Nombre="";
-var Title="";
-var vision="";
-var nation="";
+var Nombre = "";
+var Title = "";
+var vision = "";
+var nation = "";
 
-var color="";
+var color = "";
+
+var id = "";
 
 
 
@@ -37,7 +39,7 @@ function ConexionAPI() {
 
         TraerDatosPj();
         //createCard();
-        
+
 
 
         //console.log(body)
@@ -51,7 +53,7 @@ function TraerDatosPj() {
 
     for (let index = 0; index < VectorNombres.length; index++) {
         var url = `https://api.genshin.dev/characters/${VectorNombres[index]}`;
-        image=`https://api.genshin.dev/characters/${VectorNombres[index]}/icon.png`
+        image = `https://api.genshin.dev/characters/${VectorNombres[index]}/icon.png`
 
         fetch(url)
             .then(response => response.json())
@@ -59,16 +61,17 @@ function TraerDatosPj() {
             .catch(error => console.log(error))
 
         var mostrarData = (data) => {
-                console.log(data);
+            console.log(data);
 
-            Nombre=data.name;
-            Title=data.title;
-            vision=data.vision;
-            nation=data.nation;
-            
+            Nombre = data.name;
+            id = data.name;
+            Title = data.title;
+            vision = data.vision;
+            nation = data.nation;
 
 
-            createCard(Nombre,Title,vision,nation,image);
+
+            createCard(Nombre, Title, vision, nation, image);
 
             //console.log(body)
         }
@@ -79,65 +82,80 @@ function TraerDatosPj() {
 }
 
 
-function createCard(texto1,texto2,texto3,texto4) {
+function createCard(texto1, texto2, texto3, texto4) {
 
-switch(texto3){
+    switch (texto3) {
 
-    case "Dendro":
-    color="#95EB4E";
-    break;
+        case "Dendro":
+            color = "#95EB4E";
+            break;
 
-    case "Geo":
-    color="#F9D65D";
-    break;
+        case "Geo":
+            color = "#F9D65D";
+            break;
 
-    case "Pyro":
-    color="#F74D32";
-    break;
+        case "Pyro":
+            color = "#F74D32";
+            break;
 
-    case "Anemo":
-    color="#32F7DF";
-    break;
+        case "Anemo":
+            color = "#32F7DF";
+            break;
 
-    case "Electro":
-    color="#9165F6";
-    break;
+        case "Electro":
+            color = "#9165F6";
+            break;
 
-    case "Cryo":
-    color="#65A7F6";
-    break;
+        case "Cryo":
+            color = "#65A7F6";
+            break;
 
-    case "Hydro":
-    color="#9CC9F1";
-    break;
+        case "Hydro":
+            color = "#9CC9F1";
+            break;
 
-    default:
-    color="#F0F5EC";
-    
-}
+        default:
+            color = "#F0F5EC";
+
+    }
 
 
     let card = document.getElementById("row row-cols-1 row-cols-md-2");
 
-    
-        card.innerHTML += `   <div class="col mb-4">
+
+    card.innerHTML += `   <div class="col mb-4">
         <div class="card" id="card" style="background: ${color};">
             <div class="card-body" id="cardbody">
             <div class="container">
                        
-            <img src="https://api.genshin.dev/characters/${texto1.toLowerCase().replace(" ","-")}/icon.png"  class="rounded-circle" alt="Cinque Terre" width="100" height="100"> 
+            <img src="https://api.genshin.dev/characters/${texto1.toLowerCase().replace(" ", "-")}/icon.png"  class="rounded-circle" alt="Cinque Terre" width="100" height="100"> 
           </div>
-                <h5 class="card-title">Nombre Personaje: ${texto1}</h5> 
+                <h5 class="card-title" id="${id}">Nombre Personaje: ${texto1}</h5> 
                 <p class="card-text">${texto2}.</p>
                 <p class="card-text" id="atributo">${texto3}.</p>
                 <p class="card-text">${texto4}.</p>
+                
             </div>
     
         </div>
     </div>
     `;
 
-  
 
+
+}
+
+function myFunction() {
+    
+    var x = document.getElementById("creditPage");
+    var y = document.getElementById("mainPage");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display="none"
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
 }
 
